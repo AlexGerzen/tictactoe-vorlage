5bfb357998835e153a92b3ab5ac0456d1e9dffad
Alex Gerzen <a.gerzen@yahoo.de>
2023-03-27 18:28:06 +0200

    Added Win check

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function fillShape(id) {
 
     fields[id] = currentShape;
     draw();
+    checkForWin();
 }
 
 function draw() {
@@ -21,5 +22,38 @@ function draw() {
         if(fields[i] == 'cross') {
             document.getElementById(`cross-${i}`).classList.remove('d-none');
         }
+    }
+}
+
+function checkForWin() {
+    let winner;
+
+    if (fields[0] == fields[1] && fields[0] == fields[2] && fields[0]) {
+        winner = fields[0];
+    }
+    if (fields[3] == fields[4] && fields[3] == fields[5] && fields[3]) {
+        winner = fields[3];
+    }
+    if (fields[6] == fields[7] && fields[6] == fields[8] && fields[6]) {
+        winner = fields[6];
+    }
+    if (fields[0] == fields[3] && fields[0] == fields[6] && fields[0]) {
+        winner = fields[0];
+    }
+    if (fields[1] == fields[4] && fields[1] == fields[7] && fields[1]) {
+        winner = fields[1];
+    }
+    if (fields[2] == fields[5] && fields[2] == fields[8] && fields[2]) {
+        winner = fields[2];
+    }
+    if (fields[0] == fields[4] && fields[0] == fields[8] && fields[0]) {
+        winner = fields[0];
+    }
+    if (fields[2] == fields[4] && fields[2] == fields[6] && fields[2]) {
+        winner = fields[2];
+    }
+
+    if(winner) {
+         console.log(`${winner} hat gewonnen`)
     }
 }
